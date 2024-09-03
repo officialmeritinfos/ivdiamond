67c927e056bc03d0fb4ba54f29340ec49e729d9b
Anon Dev <michaelerastus9@gmail.com>
2024-09-03 09:09:30 +0100

    Updated new investment

--- resources/views/user/new_investments.blade.php
+++ resources/views/user/new_investments.blade.php
@@ -53,10 +53,10 @@
                             </select>
                         </div>
                         <div class="form-group col-md-12 mx-auto">
-                            <label for="inputAddress2">Account</label>
+                            <label for="inputAddress2">Payment Method</label>
                             <select type="number" class="form-control form-control-lg" id="inputAddress2"
                                     name="account">
-                                <option value="">Select a Account</option>
+                                <option value="">Select an Option </option>
                                 <option value="1">New Deposit</option>
                                 @if($user->canCompound==1)
                                     <option value="2">Reinvest from Account Balance</option>
